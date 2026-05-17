5fd61b2f9908f90b83eec286a484d9321e2f63ee
kenjishir-cmd <kenjishir@gmail.com>
2026-05-17 09:10:39 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,21 +9,14 @@ self.addEventListener('install',e=>{
   self.skipWaiting();
 });
 self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>
-      Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
-    )
-  );
   self.clients.claim();
 });
 self.addEventListener('fetch',e=>{
   e.respondWith(
-    caches.match(e.request).then(r=>
-      r||fetch(e.request).then(res=>{
-        const clone=res.clone();
-        caches.open(CACHE).then(c=>c.put(e.request,clone));
-        return res;
-      }).catch(()=>caches.match('/cc_payment_tracker.html/index.html'))
-    )
+    fetch(e.request).then(res=>{
+      const clone=res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,clone));
+      return res;
+    }).catch(()=>caches.match(e.request))
   );
 });
